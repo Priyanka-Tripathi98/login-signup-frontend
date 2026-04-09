0945187268e2a88b0c084a033f1d31797e84f8f1
Priyanka-Tripathi98 <priyankatripa35@gmail.com>
2026-04-09 18:21:35 +0530

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import {HashRouter, Routes, Route} from "react-router-dom"
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Signup from "./pages/Signup"
 import Login from "./pages/Login"
 import { Dashboard}  from "./pages/Dashboard"
 import Product from "./pages/Product"
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element ={<Dashboard/>}/>
         <Route path="/product" element ={<Product/>}/>
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   )
 }
 
